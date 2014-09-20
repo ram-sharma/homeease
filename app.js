7b3c0e4b615bcdafce02618b18688cc89bbc5087
Mayank Gulati <mayank_gulati@hotmail.com>
2014-09-20 19:36:10 -0400

    fixed for express 4.0

--- app.js
+++ app.js
@@ -9,26 +9,30 @@ var app = express(),
   path = require('path'),
   url = require('url');
   
-app.configure(function() {
-  app.set('port', process.env.PORT || 80);
-  app.use(express.favicon());
-  app.use(app.router);
-});
+app.set('port', 80);
 
 app.get('/toasteroven/set/:temp', function(req, res, next){
-  return res.send(myToasterOven.setTemp(req.params.temp));
+  myToasterOven.setTemp(req.params.temp, function (statusCode, data) {
+    res.status(statusCode).send(data);
+  });
 });
 
 app.get('/toasteroven/off', function(req, res, next){
-  return res.send(myToasterOven.off());
+  myToasterOven.off(function (statusCode, data){
+    res.status(statusCode).send(data);
+  })
 });
 
 app.get('/toasteroven/broil', function(req, res, next){
-  return res.send(myToasterOven.broil());
+  myToasterOven.broil(function (statusCode, data) {
+    res.status(statusCode).send(data);
+  });
 });
 
 app.get('/toasteroven/getDoor', function(req, res, next){
-  myToasterOven.doorStatus(res);
+  myToasterOven.doorStatus(function (statusCode, data) {
+    res.status(statusCode).send(data);
+  });
 });
 
 function toasterOven(deviceID, token) {
@@ -40,32 +44,35 @@ function toasterOven(deviceID, token) {
   _this.updating = false;
   _this.knob = null;
   
-  _this.off = function() {
+  _this.off = function(cb) {
     sparkPost(_this.parentToken, _this.did, "knob", "0");
     _this.knob = 0;
+    cb(200,"Oven set to off");
   }
 
-  _this.broil = function() {
+  _this.broil = function(cb) {
     sparkPost(_this.parentToken, _this.did, "knob", "180");
-    _this.knob = "broil"
+    _this.knob = "broil";
+    cb(200,"Oven set to broil");
   }
 
-  _this.setTemp = function (temp) {
+  _this.setTemp = function (temp, cb) {
     temp = parseInt(temp);
     if (temp !== undefined && temp > 199 && temp < 451) {
       _this.knob = temp;
       angle = 50 + 80*((temp-200)/250);
       sparkPost(_this.parentToken, _this.did, "knob", angle.toString());
-    }
+      cb(200,"oven set to " + temp.toString());
+    } else cb(400,"Please use a valid temperature, between 200 and 450");
   }
 
-  _this.doorStatus = function (res) {
+  _this.doorStatus = function (cb) {
     sparkGet(_this.parentToken, _this.did, "door", function(error, response, body) {
       body = JSON.parse(body);
       if (body !== undefined && body.result !== undefined) {
-        if (body.result == 0) res.send("open");
-        else if (body.result == 1) res.send("closed");
-      } else res.send("error");
+        if (body.result == 0) cb(200,"open");
+        else if (body.result == 1) cb(200,"closed");
+      } else cb(500,"error getting data from the spark");
     });
   }
 }
