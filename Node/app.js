6cb0ee752ffa6a6e89be112981d92473f1e04627
Mayank Gulati <mayank_gulati@hotmail.com>
2014-09-20 18:02:59 -0400

    changes

--- Node/app.js
+++ Node/app.js
@@ -1,6 +1,6 @@
 var _ToasterOvenId = "55ff6c065075555339401487";
 var _AustinAccessToken = "76a42c019867165fc0f527a59ca90b72de7b3a21";
-var myToasterOven = new toasterOven(_ToasterOvenId, _AustinAccessToken, 500);
+var myToasterOven = new toasterOven(_ToasterOvenId, _AustinAccessToken);
 
 var express = require('express');
 
@@ -28,44 +28,46 @@ app.get('/toasteroven/broil', function(req, res, next){
 });
 
 app.get('/toasteroven/getDoor', function(req, res, next){
-  return res.send(myToasterOven.doorStatus);
+  myToasterOven.doorStatus(res);
 });
 
-function toasterOven(deviceID, token, rate) {
+function toasterOven(deviceID, token) {
 
   _this = this;
 
   _this.did = deviceID;
   _this.parentToken = token;
-  _this.doorStatus = null;
   _this.updating = false;
+  _this.knob = null;
   
   _this.off = function() {
     sparkPost(_this.parentToken, _this.did, "knob", "0");
+    _this.knob = 0;
   }
 
   _this.broil = function() {
     sparkPost(_this.parentToken, _this.did, "knob", "180");
+    _this.knob = "broil"
   }
 
   _this.setTemp = function (temp) {
     temp = parseInt(temp);
     if (temp !== undefined && temp > 199 && temp < 451) {
+      _this.knob = temp;
       angle = 50 + 80*((temp-200)/250);
       sparkPost(_this.parentToken, _this.did, "knob", angle.toString());
     }
   }
 
-  setInterval(function() {
+  _this.doorStatus = function (res) {
     sparkGet(_this.parentToken, _this.did, "door", function(error, response, body) {
       body = JSON.parse(body);
       if (body !== undefined && body.result !== undefined) {
-        if (body.result == 0) _this.doorStatus = "open";
-        else if (body.result == 1) _this.doorStatus = "closed";
-      } else _this.doorStatus = null;
+        if (body.result == 0) res.send("open");
+        else if (body.result == 1) res.send("closed");
+      } else res.send("error");
     });
-  }, rate);
-
+  }
 }
 
 function sparkGet(token, deviceID, variable, cb) {
