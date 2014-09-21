ff9c332d02f4f9eb75260903388e1b51ff46500b
Mayank Gulati <mayank_gulati@hotmail.com>
2014-09-21 09:24:22 -0400

    corss origin

--- app.js
+++ app.js
@@ -19,6 +19,12 @@ var app = express(),
 app.use(favicon(__dirname + '/favicon.ico'));
 app.use(bodyParser.json());
 
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 app.get('/toasteroven/set/:temp', function(req, res, next){
   myToasterOven.setTemp(req.params.temp, function (statusCode, data) {
     res.status(statusCode).send(data);
